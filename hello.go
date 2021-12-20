297b48177c2ec613adea2ece5f6e8fd0e1c103cb
Waqas-Shah-42 <wshah052@uottawa.ca>
2021-12-20 11:22:36 +0500

    Added initial files for web dev

---
+++ hello.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+    r := mux.NewRouter()
+
+    r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        title := vars["title"]
+        page := vars["page"]
+        fmt.Fprintf(w, "you have requested book %s on page %s", title, page)
+    })
+
+    //fs := http.FileServer(http.Dir("static/"))
+    //http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+    http.ListenAndServe(":80", r)
+}
