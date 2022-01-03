f9ed29536436c5a597c128f9906f5292ab920ee2
w.shah <w.shah@techgenix.com.pk>
2022-01-03 20:11:29 +0500

    Added README.MD file for creating modules part

--- Creating modules/hello/hello.go
+++ Creating modules/hello/hello.go
@@ -10,20 +10,21 @@ import (
 func main() {
 
 	// Set properties of the predefined Logger, including
-    // the log entry prefix and a flag to disable printing
-    // the time, source file, and line number.
-    log.SetPrefix("greetings: ")
-    log.SetFlags(0)
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(log.LUTC | log.Ldate)
 
-    // Get a greeting message and print it.
-    message, err := greetings.Hello("")
+	// Get a greeting message and print it.
+	message, err := greetings.Hello("Waqas")
 
 	// If an error was returned, print it to the console and exit the program
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Print("This is a log statement")
 	// If no error was returned, print the returned message to the console
-    fmt.Println(message)
+	fmt.Println(message)
 }
 
 //go mod edit -replace example.com/greetings=../greetings
