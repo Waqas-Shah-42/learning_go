1652f8824ef272cf097eafb0fda856cd857e0e0b
Waqas Shah <wshah052@uottawa.ca>
2022-01-28 11:50:22 +0500

    Created a seperate function for game logic.\n Also added some additional comments

--- Quiz game/main.go
+++ Quiz game/main.go
@@ -13,11 +13,11 @@ func main() {
 	flag.Parse()
 
 	file, err := os.Open(*csvFile)
-
 	if err != nil {
 		fmt.Printf("Could not open file: %s\n", *csvFile)
 		os.Exit(1)
 	}
+
 	reader := csv.NewReader(file)
 	lines, err := reader.ReadAll()
 	if err != nil {
@@ -26,21 +26,28 @@ func main() {
 	}
 
 	problems := parseLines(lines)
+	runGame(problems)
+}
 
-	correct_counter := 0
+// Takes a struct containing problems and answers and runs the game
+func runGame(problems []problem) (error){
+	correct_ans_count := 0
+
 	for i, problem := range problems {
 		fmt.Printf("Problem #%d: %s = \n", i+1, problem.q)
 		var answer string
 		fmt.Scanf("%s\n", &answer)
+
 		if answer == problem.a {
 			fmt.Printf("Correct\n")
-			correct_counter++
+			correct_ans_count++
 		}
 	}
-
-	fmt.Printf("You got %d out of %d questions Correct.\n", correct_counter, len(problems))
+	fmt.Printf("You got %d out of %d questions Correct.\n", correct_ans_count, len(problems))
+	return nil
 }
 
+// Parses lines and returns array of stuct containins ques and ans
 func parseLines(lines [][]string) []problem {
 	ret := make([]problem, len(lines))
 
@@ -54,6 +61,7 @@ func parseLines(lines [][]string) []problem {
 	return ret
 }
 
+// Is a struct which contains both the ques and ans
 type problem struct {
 	q string
 	a string
